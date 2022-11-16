f8bd451fdd4aef6b9198b8e02414ac0d3c2abd2b
Bajrangi18 <bharatdeep.race@gmail.com>
2022-11-16 16:07:38 +0530

    build1.0

---
+++ build/precache-manifest.9ce0b1f30485d2a49a333d1b2dbe74cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "315611499331e473578e",
+    "url": "/static/css/main.0df6274a.chunk.css"
+  },
+  {
+    "revision": "315611499331e473578e",
+    "url": "/static/js/main.31561149.chunk.js"
+  },
+  {
+    "revision": "4ccff42df7664b317066",
+    "url": "/static/js/1.4ccff42d.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "e819114385e1366a98af",
+    "url": "/static/js/runtime~main.e8191143.js"
+  },
+  {
+    "revision": "c90661533024f842496ded04283a681d",
+    "url": "/static/media/logo.c9066153.png"
+  },
+  {
+    "revision": "71c2bc9f370bb450ac51e874b6acf0f4",
+    "url": "/static/media/intback.71c2bc9f.jpg"
+  },
+  {
+    "revision": "5fa0e0dbd6125dc753f5ee105cfbf756",
+    "url": "/static/media/audi.5fa0e0db.jpg"
+  },
+  {
+    "revision": "3914909361815a82fa883dc715ffc969",
+    "url": "/index.html"
+  }
+];
